274f70557d4bd3da8c1a34b7105b1d559873a750
Crowe7 <jacobocrowe@gmail.com>
2021-12-19 02:08:57 -0600

    finished overview layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 // WE HAVE DATE-FNS TO IMPORT CERTAIN FUNCTIONS FROM!
 // <button id="project" class="pro">project</button> is the HTML for the actual made projects... appended to all projects
+
+
+
+
+/*             <div class="project-board">
+                <div class="p1">
+                    <p class="header-p">low-priority</p>
+                    <div class="task-area">
+                        <button class="task">task</button>
+                    </div>
+                </div>
+                <div class="p2">
+                    <p class="header-p">in-progress</p>
+                    <div class="task-area">
+                        <button class="task">task</button>
+                    </div>
+                </div>
+                <div class="p3">
+                    <p class="header-p">completed</p>
+                    <div class="task-area">
+                        <button class="task">tasktasktasktasktasktask</button>
+                        <button class="task">task</button>
+                    </div>
+                </div>
+                <div class="p4">
+                    <p class="header-p">due-soon</p>
+                    <div class="task-area">
+                        <button class="task">task</button>
+                    </div>
+                </div>
+            </div>*/
